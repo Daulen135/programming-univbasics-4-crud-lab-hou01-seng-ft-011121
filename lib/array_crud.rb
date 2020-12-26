192b7455950917a71c45eb3d26f92867df19514d
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-26 05:13:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ x=[4,9,2]
+ x.unshift("wow")  
 end
 
 def remove_element_from_start_of_array(array)
