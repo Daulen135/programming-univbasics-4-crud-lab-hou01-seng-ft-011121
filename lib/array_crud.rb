e9a9c4d2020d0c699123cb03be4ca0006f93f4c8
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-26 04:47:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array<<(2)
+ digits=[4,9,2]
+  digits<<(2)
 end
 
 def add_element_to_start_of_array(array, element)
